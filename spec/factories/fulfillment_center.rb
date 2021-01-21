3da4f22733bf934b43d9c1c43f785fe00d1ca87e
Chanakya Devraj <chanakyadev30@gmail.com>
2021-01-21 16:29:03 +0530

    Added fulfillment center model rspec test cases

---
+++ spec/factories/fulfillment_center.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :fulfillment_center do
+    name { Faker::Name.name }
+  end
+end
---
+++ spec/models/fulfillment_center_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe FulfillmentCenter, type: :model do
+  subject { build(:fulfillment_center) }
+
+  describe 'Schema' do
+    it { is_expected.to have_db_column(:name).of_type(:string).with_options(null: false) }
+  end
+
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+  end
+end
