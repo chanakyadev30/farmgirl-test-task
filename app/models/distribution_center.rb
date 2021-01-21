24d40a41fb045e2537f99431147079e6f95890a9
Chanakya Devraj <chanakyadev30@gmail.com>
2021-01-21 16:41:09 +0530

    Added distribution center product migration and model

--- app/models/distribution_center.rb
+++ app/models/distribution_center.rb
@@ -1,4 +1,7 @@
 class DistributionCenter < ApplicationRecord
+  # associations
+  has_many :products, through: :distribution_center_products # for future reference
+
   # validations
   validates :name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  # associations
+  has_many :distribution_center, through: :distribution_center_products # for future reference
+
   # validations
   validates :name, presence: true, uniqueness: true
 end
