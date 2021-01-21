7f218ef4cf07384b86713d6bce58cb6e9830622e
Chanakya Devraj <chanakyadev30@gmail.com>
2021-01-21 16:09:38 +0530

    Added distribution center migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_161817) do
+ActiveRecord::Schema.define(version: 2021_01_22_164822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "distribution_centers", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
