e745188b229f3dcd1c6a64b2731bc74bb799a914
Chanakya Devraj <chanakyadev30@gmail.com>
2021-01-21 16:23:47 +0530

    Added fulfillment center migration and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_164822) do
+ActiveRecord::Schema.define(version: 2021_01_22_165529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "distribution_centers", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "fulfillment_centers", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
